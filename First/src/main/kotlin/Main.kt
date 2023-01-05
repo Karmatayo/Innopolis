402619cf64caf9d9cad7318be67cfc2581530dd4
Karmatayo <122013671+Karmatayo@users.noreply.github.com>
2023-01-05 23:04:32 +0500

    First

---
+++ First/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    println("Матханова Каролина, техник-программист") //задание 1
+    var age: Int = 19 //задание 2
+    print("$age\n")
+    var name = "hhhh"
+    var ff: Char = 'd'
+    var bb: Float = 2.4f
+    var gg: Double = 1.65453768
+    val number: Int = 4 //нельзя менять значения у val
+    print("$bb\n")
+    print("$ff\n")
+    print("$name\n")
+    print("$gg\n")
+    print("Имя: ")
+    var r = readLine() //задание 3
+    print("Привет, $r")
+
+}
